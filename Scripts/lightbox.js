5331a11b3821da5f1825dbabda4fe16431e33292
zacheryViralbamboo <zachery@viralbamboo.com>
2017-03-27 17:40:21 +0100

    Upload of files

---
+++ Scripts/lightbox.js
@@ -0,0 +1,29 @@
+//Hello!
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+$overlay.append($image);
+
+$overlay.append($caption);
+
+$("#work").append($overlay);
+
+$(".gallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+
+  $image.attr("src", imageLocation);
+
+  $overlay.show();
+
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+
+$overlay.click(function(){
+
+  $overlay.hide();
+});
